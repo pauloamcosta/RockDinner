a5b4bbc0aa6a144802e4c3337d6819a82f37b4ba
Paulo Andre Melo Costa <pauloamcosta2@gmail.com>
2018-06-09 02:33:46 -0300

    tela inicia, restaurantes, about. criado components e routing.

---
+++ src/app/restaurants/restaurant/restaurant.model.ts
@@ -0,0 +1,9 @@
+export interface Restaurant {
+    id: string;
+    name: string;
+    category: string;
+    deliveryEstimate: string;
+    rating: number;
+    imagePath: string;
+// tslint:disable-next-line:eofline
+}
