ff1f650c891b69faedba8d7db7852f1a2700492a
Paulo Andre Melo Costa <pauloamcosta2@gmail.com>
2018-06-10 01:30:07 -0300

    tratando erros e criando componentes menu, shopping-cart e menu-item

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -5,6 +5,8 @@ import {ROCK_API} from '../app.api';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { ErrorHandler } from '../app.error-handler';
 
 
 @Injectable()
@@ -14,6 +16,7 @@ export class RestaurantsService {
 
        restaurants(): Observable<Restaurant[]> {
            return this.http.get(`${ROCK_API}/restaurants`)
-           .map(response => response.json());
+           .map(response => response.json())
+           .catch(ErrorHandler.handleError);
          }
        }
